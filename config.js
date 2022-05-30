df5103bfe8e9b80a91cec892b1f9c7bb4240c748
nguyenduchuan03 <106550670+nguyenduchuan03@users.noreply.github.com>
2022-05-31 00:46:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     titleWeb: "Gửi Trân ",
-    introTitle: 'Who are you',
+    introTitle: 'Bảo Trân',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
