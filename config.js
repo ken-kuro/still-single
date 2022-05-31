efb005ad608fc307bb276d1937945d15805e21bc
nguyenduc_huan <106550670+nguyenduchuan03@users.noreply.github.com>
2022-05-31 10:38:04 +0700

    Update config.js

--- config.js
+++ config.js
@@ -16,5 +16,5 @@ const CONFIG = {
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/shannsayhi'
+    messLink: 'https://github.com/nguyenduchuan03/still-single/blob/main/img/giphy2.gif?raw=true'
 }
